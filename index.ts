89835afef7003ad61bad035170017e57aa66bd02
Pichilichi <garmacfco@gmail.com>
2020-11-12 10:07:59 +0100

    hecho

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
-let a;
-let b;
-let c;
+let a: number;
+let b: String;
+let c: Boolean, number;
 
 a = 5;
 b = "texto";
